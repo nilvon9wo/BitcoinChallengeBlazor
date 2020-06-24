bf034ec0601a2854f4ce3ff438acc00e1a478fc8
Brian Kessler <kessler.bm@gmail.com>
2020-06-24 05:41:51 +0200

    Fixed defect in timed operation. Also refactoring to improve DSL.

--- BitcoinChallengeBlazorApp/Program.cs
+++ BitcoinChallengeBlazorApp/Program.cs
@@ -10,12 +10,17 @@ namespace BitcoinChallengeBlazorApp {
     public class Program {
         public static async Task Main(string[] args) {
             WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+            
             builder.RootComponents.Add<App>("app");
-            AppSettings appSettings = builder.Configuration.Get<AppSettings>();
-            _ = builder.Services.AddSingleton<BitcoinChallengeSettings>(new BitcoinChallengeSettings(appSettings));
-            _ = builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            _ = builder.Services.AddSingleton<PeriodicTimer>(new PeriodicTimer());
-            await builder.Build().RunAsync();
+            _ = builder.Services.AddTransient(sp => new HttpClient { 
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+            });
+            _ = builder.Services.AddSingleton<PeriodicTimer>(
+                new PeriodicTimer(builder.Configuration.Get<AppSettings>())
+                );
+            
+            await builder.Build()
+                .RunAsync();
         }
     }
 }
--- BitcoinChallengeBlazorApp/Services/PeriodicTimer.cs
+++ BitcoinChallengeBlazorApp/Services/PeriodicTimer.cs
@@ -3,7 +3,12 @@ using System.Threading;
 
 namespace BitcoinChallengeBlazorApp.Services {
     public class PeriodicTimer {
-        public Timer Start(int RefreshTimeInSeconds, TimerCallback callback) {
+        public int RefreshTimeInSeconds { get; private set; }
+        public PeriodicTimer(AppSettings appSettings) {
+            this.RefreshTimeInSeconds = appSettings.RefreshTimeInSeconds;
+        }
+
+        public Timer Start(TimerCallback callback) {
             TimeSpan startTimeSpan = TimeSpan.Zero;
             TimeSpan periodTimeSpan = TimeSpan.FromSeconds(RefreshTimeInSeconds);
             return new Timer(callback, null, startTimeSpan, periodTimeSpan);
